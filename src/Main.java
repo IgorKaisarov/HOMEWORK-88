93da0e6d258197798904fb45d4bea354cb1f9198
IgorKaisarov <Igo444@mail.ru>
2025-08-17 10:45:05 +0600

    Work on mistakes 1.1

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,7 @@ public class Main {
         float[] secondNew = new float[]{1.57f, 7.654f, 9.986f};
         for (int i = 0; i < secondNew.length; i++) {
             if (i == secondNew.length - 1) {
-                System.out.println(secondNew[i]);
+                System.out.print(secondNew[i]);
             } else {
                 System.out.print(secondNew[i] + ", ");
             }
@@ -39,7 +39,7 @@ public class Main {
         int[] thirdNew = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         for (int g = 0; g < thirdNew.length; g++) {
             if (g == thirdNew.length - 1) {
-                System.out.println(thirdNew[g]);
+                System.out.print(thirdNew[g]);
             } else {
                 System.out.print(thirdNew[g] + ", ");
             }
